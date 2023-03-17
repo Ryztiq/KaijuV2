9f69f3d9d3554d3ce4f485f0121e9c18e86249ae
Brian R <63207674+Ryztiq@users.noreply.github.com>
2023-03-17 14:00:31 -0500

    Fixed Reset Script

--- Assets/Scripts/ActiveButton.cs
+++ Assets/Scripts/ActiveButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,79 +6,34 @@ using UnityEngine;
 
 public class ActiveButton : MonoBehaviour
 {
-    public float speed = 5f;
-    public float distance = 0.1f;
-    
-    
-    public List<GameObject> PersonTargets = new List<GameObject>();
-    private List<Vector3> PersonTargetPositions = new List<Vector3>();
-    public GameObject PersonTargetPrefab;
+    public List<TargetInfo> targetInfos = new();
 
-    public List<GameObject> Targets = new List<GameObject>();
-    private List<Vector3> TargetsPos = new List<Vector3>();
-    public GameObject TargetPrefab;
+    [Serializable]
+    public class TargetInfo
+    {
+        public List<GameObject> sceneTargets = new List<GameObject>();
+        public List<Vector3> sceneTargetPositions = new List<Vector3>();
+        public GameObject targetPrefab;
 
-    public List<GameObject> BoxTargets = new List<GameObject>();
-    private List<Vector3> BoxTargetPos = new List<Vector3>();
-    public GameObject BoxTargetPrefab;
-
-    private Vector3 originalPosition;
-
-    private bool isMovingDown = false;
-
-    // Start is called before the first frame update
+    }
     void Start()
     {
-        originalPosition = transform.position;
-        foreach (var Obj in PersonTargets)
-        {
-            PersonTargetPositions.Add(Obj.transform.position);
-        }
+        foreach (var list in targetInfos) 
+            foreach (var sceneTarget in list.sceneTargets) list.sceneTargetPositions.Add(sceneTarget.transform.position);
     }
 
-    public void ResetTargets()
+    private IEnumerator TargetReset()
     {
-        StartCoroutine(Delay());
+        yield return new WaitForSeconds(2);
         //Person targets
-        if (PersonTargets.Count >0) foreach (var target in PersonTargets) Destroy(target);
-        
-        for (int i = 0; i < PersonTargetPositions.Count; i++)
+        foreach (var list in targetInfos)
         {
-            Instantiate(PersonTargetPrefab, PersonTargetPositions[i], Quaternion.identity);
-        }
-
-        //Targets
-        if (Targets.Count > 0) foreach (var target in Targets) Destroy(target);
-
-        for (int i = 0; i < TargetsPos.Count; i++)
-        {
-            Instantiate(TargetPrefab, TargetsPos[i], Quaternion.identity);
-        }
-
-
-        //Boxes
-        if (BoxTargets.Count > 0) foreach (var target in BoxTargets) Destroy(target);
-
-        for (int i = 0; i < BoxTargetPos.Count; i++)
-        {
-            Instantiate(BoxTargetPrefab, BoxTargetPos[i], Quaternion.identity);
-        }
-    }
-    
-
-    
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-        if (isMovingDown)
-        {
-            float step = speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, originalPosition - new Vector3(0f, distance, 0f), step);
-            if (transform.position == originalPosition - new Vector3(0f, distance, 0f))
+            if(list.sceneTargets.Count > 0) foreach (var target in list.sceneTargets) if(target != null) Destroy(target);
+            list.sceneTargets.Clear();
+            for (int i = 0; i < list.sceneTargetPositions.Count; i++)
             {
-                isMovingDown = false;
+                GameObject target = Instantiate(list.targetPrefab, list.sceneTargetPositions[i], Quaternion.identity);
+                list.sceneTargets.Add(target);
             }
         }
     }
@@ -85,28 +41,12 @@ public class ActiveButton : MonoBehaviour
     public void OnTriggerEnter(Collider other)
     {
         print($"SOMETHING ENTERED: {other.gameObject.name}");
-        if (other.gameObject.CompareTag("Bullet")) if(!LeanTween.isTweening(gameObject))ButtonAnimation();
+        if (other.gameObject.CompareTag("Bullet") && !LeanTween.isTweening(gameObject)) ButtonAnimation();
     }
 
-   public void IsMoving()
+    public void ButtonAnimation()
     {
-        isMovingDown = true;
-    }
-   
-   public void ButtonAnimation()
-    {
-        LeanTween.moveY(gameObject, transform.position.y - 0.06f, 0.1f).setEase(LeanTweenType.easeInOutSine).setLoopPingPong(1).setOnComplete(() => ResetTargets());
+        LeanTween.moveY(gameObject, transform.position.y - 0.06f, 0.1f).setEase(LeanTweenType.easeInOutSine).setLoopPingPong(1).setOnComplete(() => StartCoroutine(TargetReset()));
     }
 
-    public void ResetPosition()
-    {
-        transform.position = originalPosition;
-        isMovingDown = false;
-    }
-
-    IEnumerator Delay()
-    {
-        yield return new WaitForSeconds(2);
-    }
-   
 }
