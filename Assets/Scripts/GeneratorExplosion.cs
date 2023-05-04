7f5fd9693067ad7040c8856ab3e8ef800f79dfed
Baerutt <barrettury@gmail.com>
2023-05-04 15:43:13 -0500

    Explosion Editing
    ~Changed script so that once it explodes it deletes the material connected to the Generator. This is a temporary solution, but it also means that it has no material. I've tried to find a solution to this, but so far no luck, as no matter what I do the material will not change from Cyan
    _Deleted/ Edited the DOTween settings

--- Assets/Scripts/GeneratorExplosion.cs
+++ Assets/Scripts/GeneratorExplosion.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System.Net;
+using Unity.XR.CoreUtils;
 
 public class GeneratorExplosion : MonoBehaviour
 {
-    bool hasExploded = false;
+    //https://www.youtube.com/watch?v=AbHTpZmaVpk
+
+    public bool hasExploded = false;
+    public bool setColor = false;
     public ParticleSystem Explosion;
     public ParticleSystem Smoke;
     public AudioSource konosuba;
-
+    
+    
+    public GameObject glowy;
     public Material emission;
     // Start is called before the first frame update
     void Start()
     {
-        
+        glowy = this.gameObject.transform.GetChild(0).gameObject;
+        emission = glowy.GetComponent<MeshRenderer>().material;
         Explosion.Pause();
         Smoke.Pause();
         
@@ -23,19 +31,25 @@ public class GeneratorExplosion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if(setColor)
+        //{
+        //    emission.SetColor("_EmissionColor", Color.black * 0);
+        //}
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        
-        if(collision.gameObject.CompareTag("DroneBullet"))
+        if (!hasExploded)
         {
-            ColorChange();
-            StartCoroutine(Explode());
+            if (collision.gameObject.CompareTag("DroneBullet"))
+            {
+                ColorChange();
+                StartCoroutine(Explode());
+                hasExploded = true;
+            }
         }
     }
-
+     
     private void ColorChange()
     {
         emission.DOColor(new Color(255, 52, 25), 2);
@@ -43,17 +57,21 @@ public class GeneratorExplosion : MonoBehaviour
 
     private IEnumerator Explode()
     {
-        yield return new WaitForSeconds(2);
-        if (!hasExploded)
-        {
+        yield return new WaitForSeconds(1.0f);
 
 
-            Explosion.Play();
-            Smoke.Play();
-            konosuba.Play();
-            emission.color = Color.black;
+        Explosion.Play();
+        Smoke.Play();
+        konosuba.Play();
+
+        yield return new WaitForSeconds(1.0f);
+        //emission.SetColor("_EmissionColor", Color.black * 0);
+        setColor = true;
+        Destroy(emission);
 
 
-        }
+
+
+
     }
 }
