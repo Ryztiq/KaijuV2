21c8b634f42fa71a79cce47945d0c4e6f743bbb7
XandalS <93745805+XandalS@users.noreply.github.com>
2023-05-05 14:07:29 -0500

    Attempt to resolve conflicts 3

---
+++ Assets/Racketintator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racketintator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody Thisrb;
+    public int bounceIntensity = 100;
+    public AudioClip hitSound;
+    public AudioSource audioSource;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("DroneBullet") || collision.gameObject.CompareTag("DroneBulletBig"))
+        {
+            audioSource.PlayOneShot(hitSound);
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            rb.useGravity = true;
+            Vector3 contactNormal = collision.contacts[0].normal;
+            rb.AddForce(contactNormal*Thisrb.velocity.magnitude * bounceIntensity, ForceMode.Impulse);
+        }
+    }
+}
